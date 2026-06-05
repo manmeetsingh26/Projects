17fcc042444c08345113e67c0fe7e409f7544c53
Manmeet Singh <manmeet110907@gmail.com>
2026-06-05 19:02:42 +0530

    Added expense history feature

--- Expense-Tracker/main.cpp
+++ Expense-Tracker/main.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
     int choice;
+    double expenses[100];
+    int expenseCount = 0;
     double totalExpense = 0;
     double amount;
 
@@ -10,20 +12,28 @@ int main() {
         cout << "\n===== Expense Tracker =====\n";
         cout << "1. Add Expense\n";
         cout << "2. View Total Expense\n";
-        cout << "3. Exit\n";
+        cout << "3. View Expense History\n";
+        cout << "4. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
             case 1:
-                cout << "Enter expense amount: Rs. ";
-                cin >> amount;
+                if (expenseCount < 100) {
+                    cout << "Enter expense amount: Rs. ";
+                    cin >> amount;
 
-                if (amount > 0) {
-                    totalExpense += amount;
-                    cout << "Expense added successfully.\n";
+                    if (amount > 0) {
+                        expenses[expenseCount] = amount;
+                        expenseCount++;
+                        totalExpense += amount;
+
+                        cout << "Expense added successfully.\n";
+                    } else {
+                        cout << "Invalid amount. Please enter a positive value.\n";
+                    }
                 } else {
-                    cout << "Invalid amount. Please enter a positive value.\n";
+                    cout << "Expense limit reached.\n";
                 }
                 break;
 
@@ -32,6 +42,17 @@ int main() {
                 break;
 
             case 3:
+                if (expenseCount == 0) {
+                    cout << "No expenses added yet.\n";
+                } else {
+                    cout << "\n===== Expense History =====\n";
+                    for (int i = 0; i < expenseCount; i++) {
+                        cout << i + 1 << ". Rs. " << expenses[i] << endl;
+                    }
+                }
+                break;
+
+            case 4:
                 cout << "Thank you for using Expense Tracker.\n";
                 break;
 
@@ -39,7 +60,7 @@ int main() {
                 cout << "Invalid choice. Please try again.\n";
         }
 
-    } while (choice != 3);
+    } while (choice != 4);
 
     return 0;
 }
